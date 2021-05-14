24f586fdaa84ee10e551bf6619e59d5e704b51d4
Pavel Manakov <owfqw4a@gmail.com>
2021-05-14 16:04:54 +0700

    Add files via upload

---
+++ flask_templates/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date, datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    ird = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Article %r' % self.id
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        print(title, intro, text)
+        article = Article(title=title, intro=intro, text=text)
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except Exception as ex:
+            print(ex)
+            return 'ошибка'
+    else:
+        return render_template('create.html')
+
+
+@app.route('/user/<string:name>/<int:id>')
+def about(name, id):
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
